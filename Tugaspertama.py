bebf82f22d776b880113dcbbe9721d5ff20887d2
novitasari00 <77608953+novitasari00@users.noreply.github.com>
2021-01-18 16:58:46 +0700

    Add files via upload

---
+++ Tugaspertama.py
@@ -0,0 +1,11 @@
+print(25*"=")
+print("Program Perulangan While")
+print(25*"=")
+teks = input("Teks : ")
+ulang = int(input("Jumlah Diulang : "))
+print(25*"=")
+i=1
+while i<=ulang:
+    print (teks + " ke- " + str(i))
+    i+=1
+print(25*"=")
